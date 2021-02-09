f3845312a3e505d549f57167c67dd52e489bc24c
Samo3021 <samuelcampos15009@gmail.com>
2021-02-09 17:48:28 +0000

    Terminado

--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -1,14 +1,56 @@
 import React, { useState } from "react";
 
 export const TrafficLight = () => {
+	const [colorBox, setColorBox] = useState("");
+
 	return (
 		<div>
 			<div id="trafficTop"></div>
 			<div id="container">
-				<div className="red light"></div>
-				<div className="yellow light"></div>
-				<div className="green light"></div>
+				<div
+					className={
+						"red light " + (colorBox == "red" ? "selected" : "")
+					}
+					onClick={() => {
+						setColorBox("red");
+					}}></div>
+				<div
+					className={
+						"yellow light " +
+						(colorBox == "yellow" ? "selected" : "")
+					}
+					onClick={() => {
+						setColorBox("yellow");
+					}}></div>
+				<div
+					className={
+						"green light " + (colorBox == "green" ? "selected" : "")
+					}
+					onClick={() => {
+						setColorBox("green");
+					}}></div>
 			</div>
 		</div>
 	);
 };
+
+// import React, { useState } from "react";
+// // imports
+// //create your first component
+// export function ConditionalRenderFunction() {
+// 	let colores = ["red", "blue", "green", "purple", "orange"];
+// 	let [colorBox, setColorBox] = useState("red");
+// 	const cambiarColor = () => {
+// 		let color = Math.floor(Math.random() * colores.length);
+// 		// console.log(color);
+// 		setColorBox((colorBox = colores[color]));
+// 	};
+// 	return (
+// 		<div className="color-box">
+// 			<h1>Componente Funcion</h1>
+// 			<div className="box" style={{ backgroundColor: colorBox }}></div>
+// 			<p>{colorBox}</p>
+// 			<button onClick={cambiarColor}>Cambiar Color</button>
+// 		</div>
+// 	);
+// }
